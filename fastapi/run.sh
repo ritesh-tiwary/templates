774bdbccbe9dfd40f8d56e8d1ee591067f36e9df
Ritesh Tiwary <37486223+ritesh-tiwary@users.noreply.github.com>
2024-02-21 17:24:17 +0000

    Initial commit

--- fastapi/run.sh
+++ fastapi/run.sh
@@ -1,7 +1,12 @@
+# docker ps
+# docker stop CONTAINER_ID
+# docker rm CONTAINER_ID
+
 # docker build -t fastapi.app .
 # docker run -d -p 8000:8000 fastapi.app:latest
-
 # docker-compose up -d --build --scale fastapi.app=3
 # docker compose -p fastapi_app logs
-# docker compose -p fastapi_app -f docker-compose.yml up -d --build --scale fastapi.app=3
-uvicorn app:app --host localhost --port 8000 --reload
+
+# docker compose -p fastapi_app -f docker-compose.yml up -d --build --scale fastapi.app=4
+
+uvicorn app:app --proxy-headers --host 0.0.0.0 --port 8000 --reload
